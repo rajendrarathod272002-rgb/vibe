39fc21992ebfcfabb397a6bd42408bb50c143cde
rajendrarathod272002-rgb <rajendrarathod272002@gmail.com>
2026-09-22 11:46:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // 1. Supabase Configuration (Apna URL aur Key yahan daalein)
 const SUPABASE_URL = 'https://xvgzbauxaqfkqfqslker.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2Z3piYXV4YXFma3FmcXNsa2VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MjU2MjMsImV4cCI6MjEwNTQwMTYyM30.FeEjPqSu0Rm8A7lt3qrixNkEVrsQNwlstDumLZaJDlM';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // 2. State
 let allProducts = [];
 let cart = [];
